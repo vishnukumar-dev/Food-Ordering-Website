68e3167edec69c26366596261ea7f8271dcf134c
Vishnu kumar <37202622+vishnukk11@users.noreply.github.com>
2022-04-10 23:03:46 +0530

    upload

---
+++ index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    //banner owl carousel
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots:true,
+        items:1
+    });
+
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+// Isotope Filter
+    var $grid = $(".grid").isotope({
+        itemSelector : ".grid-item",
+        layoutMode : "fitRows"
+    });
+
+    // filter Items on button click
+    $(".button-group").on("click","button",function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter : filterValue});
+    })
+
+
+
+});
